fe9b13cf09ca02d65068ea44a6aa73554f9795d7
Octavian Teodor Voicencu <teodorvoicencu@gmail.com>
2023-10-17 12:25:01 +0300

    Create person model

--- backend/people/admin.py
+++ backend/people/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "email", "created_at", "updated_at"]
+    search_fields = ["id", "first_name", "last_name", "email"]
+    ordering = ["updated_at"]
--- backend/people/models.py
+++ backend/people/models.py
@@ -1,3 +1,28 @@
+from decimal import Decimal
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Trackable(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class Person(Trackable):
+    class Genders(models.TextChoices):
+        MALE = "M", "MALE"
+        FEMALE = "F", "FEMALE"
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(null=True, blank=True)
+    gender =  models.CharField(max_length=3, choices=Genders.choices, blank=True, null=True)
+    date_of_birth = models.DateField()
+    industry = models.CharField(max_length=150, blank=True, null=True)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))], blank=True, null=True)
+    years_of_experience = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
